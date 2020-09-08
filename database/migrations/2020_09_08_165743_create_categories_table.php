be5ce7c3e02cb56d165e97eefdb506b4d2134b26
hiroki-it <hasegawafeedshop@gmail.com>
2020-09-09 04:05:38 +0900

    add カテゴリーテーブルをマイグレーションで生成できるようにした．

---
+++ database/migrations/2020_09_08_165743_create_categories_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+
+            $table->bigIncrements('id')
+                ->comment('ブログID');
+
+            $table->string('title', 255)
+                ->comment('タイトル');
+
+            $table->mediumText('body')
+                ->comment('本文');
+
+            $table->dateTime('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'))
+                ->comment('作成日');
+
+            $table->dateTime('updated_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))
+                ->comment('更新日');
+
+            $table->boolean('is_deleted')
+                ->comment('削除フラグ');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
