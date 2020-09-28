44b87fb3105b71e5942cbf6597cd43f4d956e0e7
hiroki-it <hasegawafeedshop@gmail.com>
2020-09-29 03:58:12 +0900

    add LaravelのModelを継承したエンティティ抽象クラスを実装した．

---
+++ app/Domain/Entity/Entity.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * エンティティ抽象クラス
+ */
+abstract class Entity extends Model
+{
+
+}
