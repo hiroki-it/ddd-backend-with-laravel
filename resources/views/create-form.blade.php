a0618180ece949be8e73f1406cc7e3ad61c27b0f
hiroki-it <hasegawafeedshop@gmail.com>
2020-09-29 03:50:03 +0900

    fix エラーメッセージの位置を表示方法を修正した．

--- resources/views/create-form.blade.php
+++ resources/views/create-form.blade.php
@@ -7,53 +7,45 @@
     {{-- ローカル静的ファイルは，assetヘルパーを使用 --}}
     <link rel="stylesheet" href="{{ asset('/css/post-form.css') }}">
 </head>
-
 <body>
-
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h2>記事投稿フォーム</h2>
             <form method="POST" action="{{ url('/article') }}">
-                {{--CSRF対策用にトークンを生成--}}
+                <!-- CSRF対策用にトークンを生成 -->
                 @csrf
                 <div class="form-group">
-                    <label for="created-at">投稿日時</label>
+                    <label for="created-at">投稿日時<span class="badge badge-danger">必須</span></label>
                     <input id="created-at" class="form-control" name="created_at" size="20" value="" placeholder="日付を入力します。">
                 </div>
-
                 <div class="form-group">
-                    <label for="title">タイトル</label>
+                    <label for="title">タイトル<span class="badge badge-danger">必須</span></label>
                     <input id="title" class="form-control" name="title" value="" placeholder="タイトルを入力します。">
                 </div>
-
                 <div class="form-group">
-                    <label for="category-type">カテゴリ</label>
+                    <label for="category-type">カテゴリ<span class="badge badge-danger">必須</span></label>
                     <select id="category-type" class="form-control" name="category_type">
                         <option value="" selected disabled>カテゴリを選択します。</option>
                         <option value="1">PHP</option>
                     </select>
                 </div>
-
                 <div class="form-group">
-                    <label for="content">本文</label>
+                    <label for="content">本文<span class="badge badge-danger">必須</span></label>
                     <textarea id="content" class="form-control" name="body" rows="15" placeholder="本文を入力してください。"></textarea>
                 </div>
-
+<!-- エラーメッセージ -->
+@if (count($errors))
+                <div>
+                    @foreach ($errors->all() as $error)
+                        <p class="alert alert-danger" role="alert">{{ $error }}</p>
+                    @endforeach
+                </div>
+@endif
                 <input type="submit" class="btn btn-primary btn-sm" value="投稿">
             </form>
-            @if (count($errors))
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
         </div>
     </div>
 </div>
-
 </body>
 </html>
