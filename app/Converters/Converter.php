5cdc45e6f316b7d8c5e49d0b13b45b3c5f7ce06a
hiroki-it <hasegawafeedshop@gmail.com>
2020-09-29 03:56:31 +0900

    update データ型変換抽象クラスの共通メソッドで，返却値の型を指定するようにした．

--- app/Converters/Converter.php
+++ app/Converters/Converter.php
@@ -3,6 +3,8 @@
 
 namespace App\Converters;
 
+use App\Domain\ValueObject\Type\Type;
+
 /**
  * データ型変換抽象クラス
  */
@@ -12,6 +14,7 @@ abstract class Converter
      * 区分クラスに変換します．
      *
      * @param string $value
+     * @return Type
      */
-    abstract public function convert(string $value);
+    abstract public function convert(string $value): Type;
 }
