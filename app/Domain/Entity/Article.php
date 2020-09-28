5352cb0794cad141e21fbc84c7179a1f25ec40a4
hiroki-it <hasegawafeedshop@gmail.com>
2020-09-29 03:58:39 +0900

    update Modelではなくエンティティ抽象クラスを継承するようにした．

--- app/Domain/Entity/Article.php
+++ app/Domain/Entity/Article.php
@@ -1,14 +1,13 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Domain\Entity;
 
-use Illuminate\Database\Eloquent\Model;
-
 /**
  * 記事クラス
  */
-final class Article extends Model
+final class Article extends Entity
 {
 
 }
