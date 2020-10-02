771701059b333373c1876712075b282291b02839
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-03 01:30:45 +0900

    update 記事のCRUD処理を実装した．

--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -3,15 +3,33 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Article;
 
+use App\Criteria\ArticleCriteria;
+use App\Domain\Repositories\ArticleRepository;
+use App\Domain\ValueObject\Id\ArticleId;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Response;
 
 /**
  * 記事コントローラクラス
+ *
+ * NOTE: ドメイン層のインターフェースを介してエンティティに依存するようにするため，ここでインスタンスを生成しない．
  */
 class ArticleController extends Controller
 {
+    /**
+     * @var ArticleRepository
+     */
+    private $articleRepository;
+
+    /**
+     * @param ArticleRepository $articleRepository
+     */
+    public function __construct(ArticleRepository $articleRepository)
+    {
+        $this->articleRepository = $articleRepository;
+    }
+
     /**
      * 記事投稿フォームを表示します．
      *
@@ -28,39 +46,66 @@ class ArticleController extends Controller
      * 記事を表示します．
      *
      * @param ArticleRequest $request
+     * @return Response
      */
-    public function getArticle(ArticleRequest $request)
+    public function findArticle(ArticleRequest $request)
     {
+        $validated = $request->validated();
+        $articleCriteria = ArticleCriteria::build($validated);
+        $article = $this->articleRepository->findWithCriteria($articleCriteria)->toArray();
 
+        return response()
+            ->view('article-list', $article)
+            ->setStatusCode(200);
     }
 
     /**
      * 記事を作成します．
      *
      * @param ArticleRequest $request
+     * @return Response
      */
     public function createArticle(ArticleRequest $request)
     {
+        $validated = $request->validated();
+        $articleCriteria = ArticleCriteria::build($validated);
 
+        $article = $this->articleRepository->findWithCriteria($articleCriteria);
+        $this->articleRepository->create($article);
+
+        return response()
+            ->setStatusCode(200);
     }
 
     /**
      * 記事を更新します．
      *
      * @param ArticleRequest $request
+     * @param ArticleId      $articleId
+     * @return Response
      */
-    public function updateArticle(ArticleRequest $request)
+    public function updateArticle(ArticleRequest $request, ArticleId $articleId)
     {
+        $validated = $request->validated();
+        $article = $this->articleRepository->findWithId($articleId);
+        $article->content = $validated['content'];
+        $this->articleRepository->update($article);
 
+        return response()
+            ->setStatusCode(200);
     }
 
     /**
      * 記事を削除します．
      *
-     * @param ArticleRequest $request
+     * @param ArticleId $articleId
+     * @return Response
      */
-    public function deleteArticle(ArticleRequest $request)
+    public function deleteArticle(ArticleId $articleId)
     {
+        $this->articleRepository->delete($articleId);
 
+        return response()
+            ->setStatusCode(200);
     }
 }
