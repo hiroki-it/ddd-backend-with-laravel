fa8a01a47cca31e783103c37d32609dae31f9a30
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-04 04:49:47 +0900

    update コンストラクタインジェクションを実装した．

---
+++ app/Domain/ValueObject/Type/ArticleType.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\ValueObject\Type;
+
+use BenSampo\Enum\Exceptions\InvalidEnumMemberException;
+
+/**
+ * 記事区分クラス
+ */
+final class ArticleType extends Type
+{
+    const PHP = '1';
+
+    /**
+     * コンストラクタインジェクション
+     *
+     * @param string $value
+     * @throws InvalidEnumMemberException
+     */
+    public function __construct(string $value)
+    {
+        parent::__construct($value);
+    }
+}
