31f89525767a898b9cfb9425d3af3416efc92059
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-11 02:09:35 +0900

    update クラスの不要な読み込みを削除した．

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
