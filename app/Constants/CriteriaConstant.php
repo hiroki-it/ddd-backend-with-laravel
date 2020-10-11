b142f23bb48aa9339870615b60cae9569c045c0d
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-12 00:24:37 +0900

    add 検索条件定数クラスを実装した．

---
+++ app/Constants/CriteriaConstant.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Constants;
+
+/**
+ * 検索条件定数クラス
+ */
+final class CriteriaConstant
+{
+    /**
+     * 順序のデフォルト値
+     */
+    public const DEFAULT_ORDER = 'asc';
+
+    /**
+     * 件数のデフォルト値
+     */
+    public const DEFAULT_LIMIT = 10;
+
+}
