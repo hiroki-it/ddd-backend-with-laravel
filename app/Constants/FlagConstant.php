04e9b7f814ca4516ea5fab91d139d9d26364f892
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-12 00:25:02 +0900

    add フラグ定数クラスを実装した．

---
+++ app/Constants/FlagConstant.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Constants;
+
+/**
+ * フラグ定数クラス
+ */
+final class FlagConstant
+{
+    /**
+     * フラグが立っている状態
+     */
+    public const IS_ON = 1;
+
+    /**
+     * フラグが立っていない状態
+     */
+    public const IS_OFF = 0;
+}
