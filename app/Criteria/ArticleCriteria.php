c6b7c803dd9179490e30301fefe298a5af26708e
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-12 00:28:34 +0900

    update 引数のデフォルト値として定数を設定した．

--- app/Criteria/ArticleCriteria.php
+++ app/Criteria/ArticleCriteria.php
@@ -2,6 +2,7 @@
 
 namespace App\Criteria;
 
+use App\Constants\CriteriaConstant;
 use App\Domain\ValueObject\Id\ArticleId;
 
 /**
@@ -16,7 +17,7 @@ final class ArticleCriteria extends Criteria
      * @param string    $order
      * @param int       $limit
      */
-    public function __construct(ArticleId $id, string $order, int $limit)
+    public function __construct(ArticleId $id, int $limit = CriteriaConstant::DEFAULT_LIMIT,  string $order = CriteriaConstant::DEFAULT_ORDER)
     {
         $this->id = $id;
         $this->limit = $limit;
