b62bd0c90dfff580d05a52de8cc188eda95f21f5
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-19 03:27:47 +0900

    update マジックメソッドの使用を制限する処理を切り出し，これをトレイトとした．

--- app/Domain/Entity/Entity.php
+++ app/Domain/Entity/Entity.php
@@ -4,28 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Entity;
 
+use App\Traits\UnsupportedMagicMethodTrait;
+
 /**
  * エンティティ抽象クラス
  */
 abstract class Entity
 {
-    /**
-     * パフォーマンスの観点からマジックゲッターメソッドの使用を禁止します．
-     *
-     * @throws \Exception
-     */
-    public function __get()
-    {
-        throw new \Exception('This method is not supported');
-    }
-
-    /**
-     * パフォーマンスの観点からマジックセッターメソッドの使用を禁止します．
-     *
-     * @throws \Exception
-     */
-    public function __set()
-    {
-        throw new \Exception('This method is not supported');
-    }
+    use UnsupportedMagicMethodTrait;
 }
--- app/Domain/ValueObject/Type/Type.php
+++ app/Domain/ValueObject/Type/Type.php
@@ -2,31 +2,13 @@
 
 namespace App\Domain\ValueObject\Type;
 
+use App\Traits\UnsupportedMagicMethodTrait;
 use BenSampo\Enum\Enum;
-use BenSampo\Enum\Exceptions\InvalidEnumMemberException;
 
 /**
  * 区分抽象クラス
  */
 abstract class Type extends Enum
 {
-    /**
-     * パフォーマンスの観点からマジックゲッターメソッドの使用を禁止します．
-     *
-     * @throws \Exception
-     */
-    public function __get()
-    {
-        throw new \Exception('This method is not supported');
-    }
-
-    /**
-     * パフォーマンスの観点からマジックセッターメソッドの使用を禁止します．
-     *
-     * @throws \Exception
-     */
-    public function __set()
-    {
-        throw new \Exception('This method is not supported');
-    }
+    use UnsupportedMagicMethodTrait;
 }
