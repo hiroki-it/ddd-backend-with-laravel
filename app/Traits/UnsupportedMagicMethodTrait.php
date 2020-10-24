9e6637b7a760783cf66bf96d6ad41c6537f5a4cd
hiroki-it <hasegawafeedshop@gmail.com>
2020-10-25 03:54:32 +0900

    fix オーバーライドするために，引数を設定した．

--- app/Traits/UnsupportedMagicMethodTrait.php
+++ app/Traits/UnsupportedMagicMethodTrait.php
@@ -12,9 +12,10 @@ trait UnsupportedMagicMethodTrait
     /**
      * パフォーマンスの観点から，マジックゲッターメソッドの使用を制限します．
      *
+     * @param $name
      * @throws \Exception
      */
-    public function __get()
+    public function __get($name)
     {
         throw new \Exception('This method is not supported');
     }
@@ -22,9 +23,11 @@ trait UnsupportedMagicMethodTrait
     /**
      * パフォーマンスの観点から，マジックセッターメソッドの使用を制限します．
      *
+     * @param $name
+     * @param $value
      * @throws \Exception
      */
-    public function __set()
+    public function __set($name, $value)
     {
         throw new \Exception('This method is not supported');
     }
