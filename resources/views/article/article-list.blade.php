cc2e1158166f0267e347daf13665520092ad95d4
hiroki-it <hasegawafeedshop@gmail.com>
2020-12-22 00:53:05 +0900

    update テンプレートを更新した．

--- resources/views/article/article-list.blade.php
+++ resources/views/article/article-list.blade.php
@@ -1,4 +1,55 @@
 @extends('layout.header')
 
 @section('content')
+    <div>
+        <table class="table table-striped table-hover">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">タイトル</th>
+                <th scope="col">カテゴリ</th>
+                <th scope="col">投稿日時</th>
+                <th scope="col">更新日時</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>01</td>
+                <td>テスト</td>
+                <td>PHP</td>
+                <td>2020-10-07</td>
+                <td>2020-10-08</td>
+                <td>
+                    <a href="" class="btn btn-primary btn-sm">詳細</a>
+                    <a href="" class="btn btn-primary btn-sm">編集</a>
+                    <a href="" class="btn btn-danger btn-sm">削除</a>
+                </td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>テスト</td>
+                <td>PHP</td>
+                <td>2020-10-07</td>
+                <td>2020-10-08</td>
+                <td>
+                    <a href="" class="btn btn-primary btn-sm">詳細</a>
+                    <a href="" class="btn btn-primary btn-sm">編集</a>
+                    <a href="" class="btn btn-danger btn-sm">削除</a>
+                </td>
+            </tr>
+            <tr>
+                <td>03</td>
+                <td>テスト</td>
+                <td>PHP</td>
+                <td>2020-10-07</td>
+                <td>2020-10-08</td>
+                <td>
+                    <a href="" class="btn btn-primary btn-sm">詳細</a>
+                    <a href="" class="btn btn-primary btn-sm">編集</a>
+                    <a href="" class="btn btn-danger btn-sm">削除</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,52 +7,27 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
               crossorigin="anonymous">
-        @stack('css') <!-- 子テンプレートで個別にCSSをプッシュ -->
+    @stack('css') <!-- 子テンプレートで個別にCSSをプッシュ -->
     </head>
     <body>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-            </div>
-        </div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">記事管理サイト</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">記事一覧<span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">新規投稿</a>
+                    <a class="nav-item nav-link" href="#">その他</a>
+                </div>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input class="form-control mr-sm-2" type="search" placeholder="キーワードを入力" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
                 </form>
             </div>
         </nav>
-        @yield('content') <!-- 子テンプレートで個別に要素を出力 -->
+    @yield('content') <!-- 子テンプレートで個別に要素を出力 -->
     </body>
 </html>
