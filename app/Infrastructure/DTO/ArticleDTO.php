f2c458ed997449cb5805b1b2b15a7f8815d3d48f
hiroki-it <hasegawafeedshop@gmail.com>
2021-01-27 03:49:35 +0900

    fix 不要な実装を削除した．

--- app/Infrastructure/DTO/ArticleDTO.php
+++ app/Infrastructure/DTO/ArticleDTO.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\DTO;
 
-use App\Domain\Entity\Article\ArticleContent;
-use App\Domain\Entity\Article\ArticleTitle;
-use App\Domain\ValueObject\Id\ArticleId;
-use App\Domain\ValueObject\Type\ArticleType;
-
 /**
  * 記事DTOクラス
  */
