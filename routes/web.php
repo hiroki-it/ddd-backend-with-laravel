9a520b2a858de0c4b49fd9cd94ee6a9e46e5135b
hiroki-it <hasegawafeedshop@gmail.com>
2021-01-29 03:47:13 +0900

    update CRUD処理のためのルーティングを実装した．

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,10 @@
  *
  * RouteServiceProviderのboot()でパターンフィルタを定義している．
  */
-Route::group(['namespace' => 'Article'], (function() {
-    Route::get('/articles', 'ArticleController@findArticle');
-    Route::get('/articles/{id}', 'ArticleController@showArticle')->middleware('article.id.converter');
+Route::group(['namespace' => 'Article'], (function () {
+    Route::get('/articles', 'ArticleController@showArticleList');
+    Route::get('/articles/{id}/detail/', 'ArticleController@showArticleDetail')->middleware('article.id.converter');
+    Route::get('/articles/{id}/edited/', 'ArticleController@showEditedArticle')->middleware('article.id.converter');
     Route::post('/articles', 'ArticleController@createArticle');
     Route::put('/articles/{id}', 'ArticleController@updateArticle')->middleware('article.id.converter');
     Route::delete('/articles/{id}', 'ArticleController@deleteArticle')->middleware('article.id.converter');
