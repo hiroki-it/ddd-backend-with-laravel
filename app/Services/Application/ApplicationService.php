2aa03d4f4bfca500f334f7c146faf99e32c7cf63
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-01 01:54:30 +0900

    fix タイポを修正した．

--- app/Services/Application/ApplicationService.php
+++ app/Services/Application/ApplicationService.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Service;
+namespace App\Services\Application;
 
 /**
  * ApplicationService抽象クラス
--- app/Services/Domain/DomainService.php
+++ app/Services/Domain/DomainService.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Service;
+namespace App\Services\Domain;
 
 /**
  * DomainService抽象クラス
