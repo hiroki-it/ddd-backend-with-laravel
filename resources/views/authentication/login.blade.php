3405e2f9ff04c541047809a208ade633d3551d14
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-06 04:17:40 +0900

    update テンプレートをBlade用に修正した．

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -3,7 +3,7 @@
 @push('css') <!-- ローカル静的ファイルは，assetヘルパーを使用 -->
 <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
-<link rel="stylesheet" href="assets/css/login.css">
+<link rel="stylesheet" href={{ asset("/css/authentication/login.css") }}>
 @endpush
 
 @section('body')
@@ -12,31 +12,27 @@
             <div class="card login-card">
                 <div class="row no-gutters">
                     <div class="col-md-5">
-                        <img src="assets/images/login.jpg" alt="login" class="login-card-img">
+                        <img src={{ asset("/images/login.jpg") }} alt="login" class="login-card-img" alt="">
                     </div>
                     <div class="col-md-7">
                         <div class="card-body">
                             <div class="brand-wrapper">
-                                <img src="assets/images/logo.svg" alt="logo" class="logo">
+                                <img src={{ asset("/images/login.svg") }} alt="logo" class="logo" alt="">
                             </div>
-                            <p class="login-card-description">Sign into your account</p>
+                            <p class="login-card-description">ログイン</p>
                             <form action="#!">
                                 <div class="form-group">
-                                    <label for="email" class="sr-only">Email</label>
+                                    <label for="email" class="sr-only">メールアドレス</label>
                                     <input type="email" name="email" id="email" class="form-control" placeholder="Email address">
                                 </div>
                                 <div class="form-group mb-4">
-                                    <label for="password" class="sr-only">Password</label>
+                                    <label for="password" class="sr-only">パスワード</label>
                                     <input type="password" name="password" id="password" class="form-control" placeholder="***********">
                                 </div>
                                 <input name="login" id="login" class="btn btn-block login-btn mb-4" type="button" value="Login">
                             </form>
-                            <a href="#!" class="forgot-password-link">Forgot password?</a>
-                            <p class="login-card-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
-                            <nav class="login-card-footer-nav">
-                                <a href="#!">Terms of use.</a>
-                                <a href="#!">Privacy policy</a>
-                            </nav>
+                            <a href="#!" class="forgot-password-link">パスワードを忘れましたか？</a>
+                            <p class="login-card-footer-text"><a href="#!" class="text-reset">アカウントを作成します．</a></p>
                         </div>
                     </div>
                 </div>
