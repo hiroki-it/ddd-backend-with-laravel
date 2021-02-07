793d9db51e1dc8c5211769bcaf0f3a09c2baca56
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-07 23:34:44 +0900

    update ファサードを使用するようにした．

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $env = config("app.env");
-
-        if($env === "local"){
+        if(App::environment("local")){
             $this->call([
                 ArticleSeeder::class,
                 UserSeeder::class,
