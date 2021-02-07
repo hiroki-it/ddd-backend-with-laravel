9bde1a844886d49fe8b3cbd785e5c3f3ba206855
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-08 00:24:01 +0900

    update アプリケーション層ではFacadeを使用しないようにした．

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * コントローラクラス
+ *
+ * アプリケーション層ではFacadeを使用しないでください．
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * アプリケーションクラス
+     *
+     * @var App
+     */
+    protected App $app;
+
+    /**
+     * コンストラクタインジェクション
+     *
+     * @param App $app
+     */
+    public function constructor(App $app)
+    {
+        $this->app = $app;
+    }
 }
