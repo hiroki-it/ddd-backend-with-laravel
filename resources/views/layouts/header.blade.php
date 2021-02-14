6da338abcb29da36191a28afc974016e8ffd7e38
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-15 03:31:17 +0900

    update 改行を修正した．

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,7 @@
         <meta charset="UTF-8">
         <title>tech-blog</title>
         {{-- Bootstrapの読み込みは共通 --}}
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-              integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-              crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     @stack("css") <!-- 子テンプレートで個別にCSSをプッシュ -->
     </head>
     <body>
