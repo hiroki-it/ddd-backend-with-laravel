f38ecb0e8ad37ec49880025d6462cc8564d768a5
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-15 03:35:41 +0900

    fix 不要な要素を削除した．

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         {{-- Bootstrapの読み込みは共通 --}}
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     @stack("css") <!-- 子テンプレートで個別にCSSをプッシュ -->
     </head>
     <body>
