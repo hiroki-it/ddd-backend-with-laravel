9b22075192d00bb7d7d4eef4e1d7a9b52e8702ac
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-15 03:29:58 +0900

    update bootstrapを読み込むようにした．

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -42,4 +42,5 @@
     </main>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 @endsection
