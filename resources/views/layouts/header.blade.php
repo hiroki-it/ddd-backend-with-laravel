f57c66a5f7396d0b97c3fdcffa9dc7ec27e5c742
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-15 03:31:40 +0900

    update metaタグを追加した．

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <title>tech-blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         {{-- Bootstrapの読み込みは共通 --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     @stack("css") <!-- 子テンプレートで個別にCSSをプッシュ -->
