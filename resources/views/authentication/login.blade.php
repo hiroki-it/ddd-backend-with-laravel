f06780f47a6bdf42fbe0e51d409f1da889d2d021
hiroki-it <hasegawafeedshop@gmail.com>
2021-02-16 08:44:03 +0900

    update テンプレートのデザインを変更した．

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -1,46 +1,33 @@
 @extends('layouts.header')
 
 @push('css') <!-- ローカル静的ファイルは，assetヘルパーを使用 -->
-<link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
 <link rel="stylesheet" href={{ asset("/css/authentication/login.css") }}>
 @endpush
 
 @section('body')
-    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div class="container">
-            <div class="card login-card">
-                <div class="row no-gutters">
-                    <div class="col-md-5">
-                        <img src={{ asset("/images/login.jpg") }} alt="login" class="login-card-img" alt="">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="brand-wrapper">
-                                <img src={{ asset("/images/login.svg") }} alt="logo" class="logo" alt="">
-                            </div>
-                            <p class="login-card-description">ログイン</p>
-                            <form method="POST" action={{ url("/login") }}>
-                            @csrf <!-- CSRF対策用にトークンを生成 -->
-                                <div class="form-group">
-                                    <label for="email" class="sr-only">メールアドレス</label>
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="">
-                                </div>
-                                <div class="form-group mb-4">
-                                    <label for="password" class="sr-only">パスワード</label>
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="">
-                                </div>
-                                <input name="login" id="login" class="btn btn-block login-btn mb-4" type="button" value="Login">
-                            </form>
-                            <p class="login-card-footer-text"><a href={{ url("/register") }} class="text-reset">アカウントを作成します．</a>
-                            </p>
-                        </div>
-                    </div>
+    <div class="login-form">
+        <form action="/examples/actions/confirmation.php" method="post">
+            <h2 class="text-center">Sign In</h2>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" placeholder="Username" required="required">
                 </div>
             </div>
-        </div>
-    </main>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                    <input type="password" class="form-control" placeholder="Password" required="required">
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block">Log in</button>
+            </div>
+            <div class="clearfix">
+                <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
+                <a href="#" class="pull-right">Forgot Password?</a>
+            </div>
+        </form>
+        <p class="text-center small">Don't have an account! <a href="#">Sign up here</a>.</p>
+    </div>
 @endsection
