4cd888bd21725f714d328835d39f0b6aaeb65f1a
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-15 23:31:17 +0900

    update ディレクトリ名を変更した．

--- app/Constant/CriteriaConstant.php
+++ app/Constant/CriteriaConstant.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Constants;
+namespace App\Constant;
 
 /**
  * 検索条件定数クラス
--- app/Constant/FlagConstant.php
+++ app/Constant/FlagConstant.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Constants;
+namespace App\Constant;
 
 /**
  * フラグ定数クラス
