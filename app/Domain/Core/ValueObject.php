31f4fe182400e6f2602b3938b9b6112886b813c1
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-20 02:34:01 +0900

    fix 不要な実装を削除した．

--- app/Domain/Core/ValueObject.php
+++ app/Domain/Core/ValueObject.php
@@ -4,36 +4,11 @@ declare(strict_types=1);
 
 namespace App\Domain\Core;
 
-use App\Traits\UnsupportedMagicMethodTrait;
-
 /**
  * 値オブジェクト抽象クラス
  */
 abstract class ValueObject
 {
-    use UnsupportedMagicMethodTrait;
-
-    /**
-     * @var string
-     */
-    private string $value;
-
-    /**
-     * @param string $value
-     */
-    public function __constructor(string $value)
-    {
-        $this->value = $value;
-    }
-
-    /**
-     * @return string
-     */
-    public function value(): string
-    {
-        return $this->value;
-    }
-
     /**
      * 値オブジェクトの等価性を検証します．
      *
