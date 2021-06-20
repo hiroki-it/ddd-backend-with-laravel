a1a04f023e1efa525b150fe724898e3de8a3c847
hiroki-it <hasegawafeedshop@gmail.com>
2021-06-20 18:50:07 +0900

    update 複合主キーを持つオブジェクトの等価性を正しく検証するために，標準の関数をオーバーライドします．

--- app/Domain/Core/Id.php
+++ app/Domain/Core/Id.php
@@ -9,5 +9,44 @@ namespace App\Domain\Core;
  */
 abstract class Id
 {
+    /**
+     * ID
+     *
+     * @var string
+     */
+    private string $id;
 
+    /**
+     * @param string $id
+     */
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * ハッシュ値を返却します．
+     *
+     * NOTE: 複合主キーを持つオブジェクトの等価性を正しく検証するために，標準の関数をオーバーライドします．
+     *
+     * @return string
+     */
+    public function hash(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * オブジェクトの等価性を検証します．
+     *
+     * NOTE: 複合複合主キーを持つオブジェクトの等価性を正しく検証するために主キーを持つオブジェクトの等価性を正しく検証するために，標準の関数をオーバーライドします．
+     *
+     * @param Id $id
+     * @return bool
+     */
+    public function equals(Id $id): bool
+    {
+        return ($id instanceof $this || $this instanceof $id) // IDオブジェクトのデータ型の等価性
+            && $this->hash() == $id->hash(); // ハッシュ値の等価性
+    }
 }
