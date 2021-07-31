348e988af75c2a20ca2d137a4ebadecd1b302568
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-01 00:54:10 +0900

    update web.phpを使用しないようにした．

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,10 +43,6 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-
-        //
     }
 
     /**
