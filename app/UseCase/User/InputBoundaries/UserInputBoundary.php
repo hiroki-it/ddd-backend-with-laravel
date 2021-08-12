da4f3e54e83b6a8354bc12670b840423ed1451b2
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-12 09:56:00 +0900

    update インターラクターがインプットバウンダリを実装するようにした．

---
+++ app/UseCase/User/InputBoundaries/UserInputBoundary.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UseCase\User\InputBoundaries;
+
+
+use App\Domain\User\Entity\User;
+use App\UseCase\User\Requests\UserCreateRequest;
+
+/**
+ * ユーザユースケースクラス
+ */
+interface UserInputBoundary
+{
+    /**
+     * ユーザを作成します．
+     *
+     * @param UserCreateRequest $input
+     */
+    public function createUser(UserCreateRequest $input): User;
+}
