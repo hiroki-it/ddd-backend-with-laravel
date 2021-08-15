b007157b4bd39cc5dc205833208df442cbe9f2d9
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-15 16:31:40 +0900

    update 条件に基づくREAD処理の結果からレスポンスモデル作成するようにした．

--- app/UseCase/Article/Responses/ArticleGetByCriteriaResponse.php
+++ app/UseCase/Article/Responses/ArticleGetByCriteriaResponse.php
@@ -2,6 +2,18 @@
 
 namespace App\UseCase\Article\Responses;
 
+/**
+ * 条件に基づく記事取得レスポンスクラス
+ */
 class ArticleGetByCriteriaResponse
 {
+    /**
+     * @var array
+     */
+    private array $articles;
+
+    public function __construct(array $articles)
+    {
+        $this->articles=$articles;
+    }
 }
