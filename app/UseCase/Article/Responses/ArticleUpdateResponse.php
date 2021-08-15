746f6fa3c5c794a33d1474e83f5e36658651e263
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-15 16:30:43 +0900

    update UPDATE処理の結果からレスポンスモデル作成するようにした．

--- app/UseCase/Article/Responses/ArticleUpdateResponse.php
+++ app/UseCase/Article/Responses/ArticleUpdateResponse.php
@@ -2,6 +2,36 @@
 
 namespace App\UseCase\Article\Responses;
 
+/**
+ * 記事更新レスポンスクラス
+ */
 class ArticleUpdateResponse
 {
+    /**
+     * @var int
+     */
+    private int $articleId;
+
+    /**
+     * @var string
+     */
+    private string $articleTitle;
+
+    /**
+     * @var string
+     */
+    private string $articleType;
+
+    /**
+     * @var string
+     */
+    private string $articleContent;
+
+    public function __construct(int $articleId, string $articleTitle, string $articleType, string $articleContent)
+    {
+        $this->articleId=$articleId;
+        $this->articleTitle=$articleTitle;
+        $this->articleType=$articleType;
+        $this->articleContent=$articleContent;
+    }
 }
