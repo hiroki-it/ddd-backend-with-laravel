042d76665c5dee792a634fc13a973e271b5fc34f
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-17 22:35:47 +0900

    update 不要な実装を削除した．

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * 認証前
- */
-Route::group(['namespace' => 'Auth'], (function () {
-    Route::get('/register', 'RegisteredUserController@create');
-    Route::post('/register', 'RegisteredUserController@store');
-    Route::get('/login', 'AuthenticatedSessionController@create');
-    Route::post('/login', 'AuthenticatedSessionController@store');
-}));
-
-/**
- * 認証後
- */
 Route::group(['middleware' => 'auth'], (function () {
 
     /**
@@ -24,7 +11,7 @@ Route::group(['middleware' => 'auth'], (function () {
         Route::get('/article/{id}', 'ArticleController@getArticle');
         Route::get('/articles', 'ArticleController@getAllArticles');
         Route::post('/articles', 'ArticleController@createArticle');
-        Route::put('/articles/{id}', 'ArticleController@updateArticle')->middleware('article.id.converter');
-        Route::delete('/articles/{id}', 'ArticleController@deleteArticle')->middleware('article.id.converter');
+        Route::put('/articles/{id}', 'ArticleController@updateArticle');
+        Route::delete('/articles/{id}', 'ArticleController@deleteArticle');
     }));
 }));
