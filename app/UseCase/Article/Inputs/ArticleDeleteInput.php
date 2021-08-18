5951ca8beaf26a6f4a3e106af7a92d7ad07c2f4e
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-18 23:37:05 +0900

    update コンストラクタを実装した．

--- app/UseCase/Article/Inputs/ArticleDeleteInput.php
+++ app/UseCase/Article/Inputs/ArticleDeleteInput.php
@@ -6,4 +6,11 @@ use App\UseCase\DeleteInput;
 
 class ArticleDeleteInput extends DeleteInput
 {
+    /**
+     * @param int $id
+     */
+    public function __construct(int $id)
+    {
+        parent::__construct($id);
+    }
 }
