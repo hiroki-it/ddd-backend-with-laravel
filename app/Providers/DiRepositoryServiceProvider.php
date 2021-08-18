062ca7fa55b50f41f18883c1b1332e9b700d504e
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-18 09:13:44 +0900

    update コントローラでインターフェースリポジトリを宣言すると，実装リポジトリの依存オブジェクト注入が自動で実行されるようにした．

---
+++ app/Providers/DiRepositoryServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * DIリポジトリサービスプロバイダクラス
+ */
+class DiRepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * サービスコンテナにクラスをバインドします．
+     *
+     * インターフェースリポジトリと実装リポジトリをバインドし，実装リポジトリのインスタンスをリゾルブします．
+     * これにより，コントローラでインターフェースリポジトリを宣言すると，実装リポジトリの依存オブジェクト注入が自動で実行されます．
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $binds = [
+            'App\Domain\Article\Repositories\ArticleRepository' => 'App\Infrastructure\Article\Repositories\ArticleRepository', // 記事リポジトリ
+            'App\Domain\User\Repositories\UserRepository' => 'App\Infrastructure\Use\Repositories\UserRepository' // ユーザリポジトリ
+        ];
+
+        foreach ($binds as $abstract => $concrete) {
+            $this->app->bind($abstract, $concrete);
+        }
+    }
+}
