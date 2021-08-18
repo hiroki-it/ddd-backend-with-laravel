9b04e41d3124bfc3179c8fd3f7e117dc287e891d
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-18 09:57:23 +0900

    update 使用しないミドルウェアの適用を外した．

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Article\ArticleController;
 
-Route::group(['middleware' => 'auth'], (function () {
-
-    Route::group(['prefix' => 'articles'], (function () {
-        Route::get('/{id}', [ArticleController::class,'getArticle']);
-        Route::get('/', [ArticleController::class,'getAllArticles']);
-        Route::post('/', [ArticleController::class,'createArticle']);
-        Route::put('/{id}', [ArticleController::class,'updateArticle']);
-        Route::delete('/{id}', [ArticleController::class,'deleteArticle']);
-    }));
+Route::group(['prefix' => 'articles'], (function () {
+    Route::get('/{id}', [ArticleController::class, 'getArticle']);
+    Route::get('/', [ArticleController::class, 'getAllArticles']);
+    Route::post('/', [ArticleController::class, 'createArticle']);
+    Route::put('/{id}', [ArticleController::class, 'updateArticle']);
+    Route::delete('/{id}', [ArticleController::class, 'deleteArticle']);
 }));
