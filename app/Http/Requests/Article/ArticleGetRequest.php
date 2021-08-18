7f97ca3767e8927fb4dc0677dd3990c612cacbed
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-18 22:47:21 +0900

    add GET処理に対してバリデーションを実行できるようにした．

---
+++ app/Http/Requests/Article/ArticleGetRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Article;
+
+use App\Constant\CriteriaConstant;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+final class ArticleGetRequest extends FormRequest
+{
+    /**
+     * @return array[]
+     */
+    public function rules(): array
+    {
+        return [
+            'order' => [
+                'required',
+                Rule::in(CriteriaConstant::ORDER_LIST)
+            ],
+            'limit' => [
+                'required',
+                Rule::in(CriteriaConstant::LIMIT_LIST)
+            ],
+        ];
+    }
+}
