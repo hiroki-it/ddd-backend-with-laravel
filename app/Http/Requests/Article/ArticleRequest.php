53b475ba489f9dcef42cec1d8764cbaf5039d17c
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-18 22:25:22 +0900

    fix 名前空間を修正した．

--- app/Http/Requests/Article/ArticleRequest.php
+++ app/Http/Requests/Article/ArticleRequest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Article;
 
 use App\Constant\CriteriaConstant;
 use Illuminate\Foundation\Http\FormRequest;
--- app/Http/Requests/User/UserRequest.php
+++ app/Http/Requests/User/UserRequest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
