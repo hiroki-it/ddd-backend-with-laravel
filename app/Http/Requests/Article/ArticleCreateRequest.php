5ecee31cf1f84bcfa1cd1e44823fb7355b18f909
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-18 22:45:56 +0900

    add CREATE処理に対してバリデーションを実行できるようにした．

---
+++ app/Http/Requests/Article/ArticleCreateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Article;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleCreateRequest extends FormRequest
+{
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'type' => [
+                'required',
+                'string'
+            ],
+            'content' => [
+                'required',
+                'string',
+                'max:10000'
+            ],
+        ];
+    }
+}
