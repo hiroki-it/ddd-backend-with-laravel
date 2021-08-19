29a66842d1fbe924bbb0cbf707aeb0d5eec3c6af
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-19 10:24:14 +0900

    update Enumが外部リポジトリに依存しないようにした．

--- app/Domain/Type.php
+++ app/Domain/Type.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace App\Domain;
 
-use BenSampo\Enum\Enum;
+use App\Traits\ImmutableTrait;
 
 /**
  * 区分抽象クラス
  */
-abstract class Type extends Enum
+abstract class Type
 {
+    use ImmutableTrait;
+
+    /**
+     * @var int
+     */
+    protected int $value;
+
+    /**
+     * @param int $value
+     */
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+    }
 }
