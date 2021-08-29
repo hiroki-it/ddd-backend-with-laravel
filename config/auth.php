f9ef0eb26fc0495ed5b5755a225a7d408e1e7980
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-29 21:20:56 +0900

    fix 名前空間を修正した．

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
           'driver' => 'eloquent',
-          'model' => App\Infrastructure\DTO\User::class,
+          'model' => App\Infrastructure\User\DTOs\UserDTO::class
         ],
 
         // 'users' => [
