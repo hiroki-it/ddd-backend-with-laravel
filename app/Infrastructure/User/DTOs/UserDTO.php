26baee34378a75eb26c86e8a539b3d7449173736
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-01 09:55:50 +0900

    update ユーザDTOをユーザエンティティに変換できるようにした．

--- app/Infrastructure/User/DTOs/UserDTO.php
+++ app/Infrastructure/User/DTOs/UserDTO.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\User\DTOs;
 
+use App\Domain\User\Entities\User;
+use App\Domain\User\Ids\UserId;
+use App\Domain\User\ValueObjects\UserEmailAddress;
+use App\Domain\User\ValueObjects\UserName;
+use App\Domain\User\ValueObjects\UserPassword;
 use App\Traits\DTOTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -59,4 +64,19 @@ class UserDTO extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * DTOをユーザエンティティに変換します．
+     *
+     * @return User
+     */
+    public function toUser(): User
+    {
+        return new User(
+            new UserId($this->user_id),
+            new UserName($this->user_name),
+            new UserEmailAddress($this->user_email_address),
+            new UserPassword($this->user_password),
+        );
+    }
 }
