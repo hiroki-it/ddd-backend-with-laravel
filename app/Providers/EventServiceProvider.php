57fca87d7016127a946c7e3ad36aaaa4380e74ea
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-04 12:59:27 +0900

    add ユーザ作成イベントとそのリスナーを新しく作成した．

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Domain\User\Events\UserCreatedEvent;
+use App\Infrastructure\User\Listeners\UserCreatedEventListener;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -14,8 +14,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        UserCreatedEvent::class => [
+            UserCreatedEventListener::class,
         ],
     ];
 
