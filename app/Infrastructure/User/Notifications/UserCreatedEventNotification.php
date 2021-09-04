219948120821bb2cc19801ad4b52f212c9cae0f3
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-04 13:00:43 +0900

    add ユーザ作成イベントの通知内容を新しく作成した．

---
+++ app/Infrastructure/User/Notifications/UserCreatedEventNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Infrastructure\User\Notifications;
+
+use App\Domain\User\Entities\User;
+use App\Infrastructure\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+final class UserCreatedEventNotification extends Notification
+{
+    /**
+     * @var User
+     */
+    private User $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return array
+     */
+    public function via(): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * @param $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage())->subject('ユーザ登録が完了いたしました')
+             ->markdown('messages.email.userCreatedEmail',[
+                 'user_name' => $this->user->name->name
+             ]);
+    }
+
+    /**
+     * @param $notifiable
+     * @return array
+     */
+    public function toArray($notifiable): array
+    {
+        return [
+            'user_id' => $this->user->id->id,
+            'user_email' => $this->user->email->email,
+        ];
+    }
+}
