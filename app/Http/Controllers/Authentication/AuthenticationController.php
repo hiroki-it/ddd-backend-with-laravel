6f6ce1072fba7430fbec39adc87a80b261e93ffd
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-05 23:36:05 +0900

    update ログアウトできるようにした．

--- app/Http/Controllers/Authentication/AuthenticationController.php
+++ app/Http/Controllers/Authentication/AuthenticationController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Authentication;
 
 use App\Http\Requests\Authentication\AuthenticationRequest;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 
 final class AuthenticationController
 {
@@ -26,5 +28,22 @@ final class AuthenticationController
 
         // 未認証ページにリダイレクトします．
         return redirect(RouteServiceProvider::UNAUTHORIZED);
+    }
+
+    /**
+     * @param AuthenticationRequest $authenticationRequest
+     * @return Application|RedirectResponse|Redirector
+     */
+    public function logout(AuthenticationRequest $authenticationRequest)
+    {
+        auth()->logout();
+
+        // セッションを削除します．
+        $authenticationRequest->session()->invalidate();
+
+        // CSRFトークンを再生成します．
+        $authenticationRequest->session()->regenerateToken();
+
+        return redirect(RouteServiceProvider::ROOT);
     }
 }
