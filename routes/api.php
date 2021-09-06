0a44a24b44ddd821c459732037d696df03468ffc
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-07 08:45:32 +0900

    update コメントを追加した．

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,19 @@ use App\Http\Controllers\Article\ArticleController;
 use App\Http\Controllers\User\UserController;
 
 Route::group(['middleware' => ['auth:web']], function () {
-
-    Route::group(['prefix' => 'users'], (function () {
+    // ユーザ
+    Route::group(['prefix' => 'users'], function () {
         Route::post('/', [UserController::class, 'createUser']);
         Route::put('/{id}', [UserController::class, 'updateUser']);
         Route::delete('/{id}', [UserController::class, 'deleteUser']);
-    }));
+    });
 
-    Route::group(['prefix' => 'articles'], (function () {
+    // 記事
+    Route::group(['prefix' => 'articles'], function () {
         Route::get('/{id}', [ArticleController::class, 'getArticle']);
         Route::get('/', [ArticleController::class, 'getAllArticles']);
         Route::post('/', [ArticleController::class, 'createArticle']);
         Route::put('/{id}', [ArticleController::class, 'updateArticle']);
         Route::delete('/{id}', [ArticleController::class, 'deleteArticle']);
-    }));
+    });
 });
