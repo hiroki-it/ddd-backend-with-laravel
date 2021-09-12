146ca6182f9d8800c291b91fe377044c539f1baf
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-12 18:55:18 +0900

    fix 不要なアノテーションを削除した．

--- database/factories/dev/ArticleFactory.php
+++ database/factories/dev/ArticleFactory.php
@@ -11,15 +11,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ArticleFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
      * @var string
      */
     protected $model = ArticleDTO::class;
 
     /**
-     * Define the model's default state.
-     *
      * @return array
      */
     public function definition()
--- database/factories/dev/UserFactory.php
+++ database/factories/dev/UserFactory.php
@@ -10,15 +10,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
      * @var string
      */
     protected $model = UserDTO::class;
 
     /**
-     * Define the model's default state.
-     *
      * @return array
      */
     public function definition()
