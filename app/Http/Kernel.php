d2d13621f4c5d4228a670954820f85a3a185c5f7
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-19 19:18:13 +0900

    fix 不要な実装を削除した．

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,9 +57,6 @@ class Kernel extends HttpKernel
         'signed'               => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle'             => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'             => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        // IdConverterクラス
-        'article.id.converter' => \App\Http\Middleware\Before\Converters\ArticleIdConverterMiddleware::class
     ];
 
     /**
