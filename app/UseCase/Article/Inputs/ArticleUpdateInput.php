632477d88d821f05dbe3297b248ecd849b85936b
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-20 14:30:57 +0900

    fix 不要な実装を削除した．

--- app/UseCase/Article/Inputs/ArticleUpdateInput.php
+++ app/UseCase/Article/Inputs/ArticleUpdateInput.php
@@ -14,11 +14,6 @@ final class ArticleUpdateInput extends UpdateInput
     protected int $id;
 
     /**
-     * @var int
-     */
-    protected int $userId;
-
-    /**
      * @var string
      */
     protected string $title;
@@ -35,15 +30,13 @@ final class ArticleUpdateInput extends UpdateInput
 
     /**
      * @param int    $id
-     * @param int    $userId
      * @param string $title
      * @param int    $type
      * @param string $content
      */
-    public function __construct(int $id, int $userId, string $title, int $type, string $content)
+    public function __construct(int $id, string $title, int $type, string $content)
     {
         $this->id = $id;
-        $this->userId = $userId;
         $this->title = $title;
         $this->type = $type;
         $this->content = $content;
