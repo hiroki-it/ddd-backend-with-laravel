eeebaa516415c847924fdfd32d55a8e9d1713e5d
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-26 19:09:12 +0900

    update 例外コードとステータスコードを区別するために，例外コードはデフォルト（ゼロ）のままとした．

--- app/Exceptions/AuthorizationException.php
+++ app/Exceptions/AuthorizationException.php
@@ -4,10 +4,12 @@ namespace App\Exceptions;
 
 use Exception;
 
+/**
+ * NOTE: Laravelの認可機能を使用していないため，例外クラスを独自定義している．
+ *
+ * 認可例外クラス
+ */
 class AuthorizationException extends Exception
 {
-    /**
-     * @var int
-     */
-    protected $code = 403;
+
 }
