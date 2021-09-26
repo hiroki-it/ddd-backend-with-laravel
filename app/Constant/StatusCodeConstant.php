d85b9a49927573176f062907ffba89365352d2bc
hiroki-it <hasegawafeedshop@gmail.com>
2021-09-26 19:09:38 +0900

    add ステータスコード定数クラスを新しく実装した．

---
+++ app/Constant/StatusCodeConstant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constant;
+
+final class StatusCodeConstant
+{
+    public const OK = 200;
+
+    public const NO_CONTENT = 204;
+
+    public const BAD_REQUEST = 400;
+
+    public const FORBIDDEN = 403;
+}
