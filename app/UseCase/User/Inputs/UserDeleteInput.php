b2fc595edbf36ee90b694cc07a5f1e2dd95b3597
hiroki-it <hasegawafeedshop@gmail.com>
2021-11-03 23:52:49 +0900

    update 認証中のユーザIDを，ユースケース層ではなく，インターフェース層で用意しておくようにした．

--- app/UseCase/User/Inputs/UserDeleteInput.php
+++ app/UseCase/User/Inputs/UserDeleteInput.php
@@ -6,4 +6,23 @@ use App\UseCase\DeleteInput;
 
 final class UserDeleteInput extends DeleteInput
 {
+    /**
+     * @var int
+     */
+    protected int $userId;
+
+    /**
+     * @var int
+     */
+    protected int $authId;
+
+    /**
+     * @param int $userId
+     * @param int $authId
+     */
+    public function __construct(int $userId, int $authId)
+    {
+        $this->userId = $userId;
+        $this->authId = $authId;
+    }
 }
--- app/UseCase/User/Inputs/UserShowInput.php
+++ app/UseCase/User/Inputs/UserShowInput.php
@@ -8,4 +8,23 @@ use App\UseCase\ShowInput;
 
 final class UserShowInput extends ShowInput
 {
+    /**
+     * @var int
+     */
+    protected int $userId;
+
+    /**
+     * @var int
+     */
+    protected int $authId;
+
+    /**
+     * @param int $userId
+     * @param int $authId
+     */
+    public function __construct(int $userId, int $authId)
+    {
+        $this->userId = $userId;
+        $this->authId = $authId;
+    }
 }
